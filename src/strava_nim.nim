4c7959a964ea4b3b49cfa57fdf3e34c77481a010
Alexander Epifanov <a.epifanov@iconic.vc>
2020-02-06 01:22:30 +0300

    README update

--- src/strava_nim.nim
+++ src/strava_nim.nim
@@ -17,23 +17,7 @@ import re
 import flatdb
 # import lmdb
 
-type
-    Interval = tuple
-        avg: float
-        start: int
-        stop: int
-
-    Pattern = tuple
-        repeat: int
-        duration: float
-
-func duration(a: Interval): float =
-    (a.stop - a.start + 1).float
-
-proc `$`(a: Interval): string =
-    let tstr = fromUnix(a.start).format("mm:ss")
-    let dstr = fromUnix(a.duration.int).format("mm:ss")
-    fmt"(avg: {a.avg}, start: {tstr}, duration: {dstr})"
+import analytic
 
 const
     clientId = "438197548914-kp6b5mu5543gdinspvt5tgj0s71q1vbv.apps.googleusercontent.com"
@@ -298,80 +282,6 @@ proc getActivity(dt: DateTime, stravaAccessToken: string): Future[(string, Table
     file.close()
     
     return (j2["name"].getStr() & " " & $j2["distance"].getFloat() & "m " & j2["type"].getStr(), t)
-
-proc `$`(d: Duration): string =
-    let d = d.toParts()
-    fmt"{d[Hours]}:{d[Minutes]:02}:{d[Seconds]:02}"
-
-proc process_best(pattern: seq[Pattern], time: seq[float], watts: seq[float]): seq[Interval] =
-    for (repeat, ws) in pattern:
-        var acc = 0.0
-        var i = 0
-        for ii, (tt, x) in time.zip(watts):
-            let t = tt.int
-            while time[i].int <= t-ws.int:
-                acc -= watts[i]
-                i += 1
-            acc += x
-            if t+1 >= ws.int:
-                let interval: Interval = (acc / ws, time[i].int, t)
-                result.add(interval)
-
-    func cmp2(a, b: Interval): int =
-        result = cmp(a.avg, b.avg)
-        if result == 0:
-            result = cmp(a.start, b.start)
-            if result == 0:
-                result = cmp(a.stop, b.stop)
-
-    result.sort(cmp2, Ascending)
-
-proc process_select(pattern: seq[Pattern], best: seq[Interval]): seq[Interval] =
-    var best = best
-    func overlap(a,b: Interval): bool =
-        if a.start <= b.start and a.stop > b.start:
-            return true
-        if b.start <= a.start and b.stop > a.start:
-            return true
-        return false
-
-    # var ps = pattern
-    # while best.len > 0 and ps.len > 0:
-    #     let x = best.pop()
-    #     for p in ps.mitems:
-    #         if x.duration >= p.duration:
-    #             block checkBlock:
-    #                 for y in result:
-    #                     if overlap(x, y):
-    #                         # echo "overlap ", x, y
-    #                         break checkBlock
-    #                 result.add(x)
-    #                 p[0] -= 1
-    #     ps.keepItIf(it[0] > 0)
-    #     echo $ps
-
-    for (p, res) in pattern.zip(best):
-        var repeat = p.repeat
-        let ws = p.duration
-        while best.len > 0 and repeat > 0:
-            let x = best.pop()
-            if x.duration >= ws:
-                block checkBlock:
-                    for y in result:
-                        if overlap(x, y):
-                            # echo "overlap ", x, y
-                            break checkBlock
-                    result.add(x)
-                    repeat -= 1
-
-    func cmp3(a, b: Interval): int =
-        result = cmp(a.start, b.start)
-        if result == 0:
-            result = cmp(a.avg, b.avg)
-            if result == 0:
-                result = cmp(a.stop, b.stop)
-
-    result.sort(cmp3, Ascending)
 
 proc process(pattern: seq[Pattern], time: seq[float], watts: seq[float]) =
     echo "processing ", pattern
