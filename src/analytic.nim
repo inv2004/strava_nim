a97ac773f1889260094b74811a7d5f2be1f65ead
AE <inv2004@gmail.com>
2020-03-17 02:39:45 +0300

    remove unused analytics

--- src/analytic.nim
+++ src/analytic.nim
@@ -103,42 +103,6 @@ func overlap*(a, b: Interval): bool =
         return true
     return false
 
-proc select_top_1*(pattern: seq[Pattern], best: seq[seq[Interval]]): seq[Interval] =
-    result.add best[0][^1]
-
-proc select_top_2*(pattern: seq[Pattern], best: seq[seq[Interval]]): seq[Interval] =
-    var m = 0.0
-
-    for x in best[0]:
-        for y in best[1]:
-            if not(x.stop <= y.start):
-                continue
-
-            let work = x.avg*15*60 + y.avg*180
-            if work > m:
-                result = @[x, y]
-                m = work
-
-proc select_top_3*(pattern: seq[Pattern], best: seq[seq[Interval]]): seq[Interval] =
-    var m = 0.0
-
-    echo "variants: ", best[0].len * (best[1].len ^ 2)
-
-    for x in best[0]:
-        for y in best[1]:
-            if not(x.stop <= y.start):
-                continue
-
-            for z in best[1]:
-                if not(y.stop <= z.start):
-                    continue
-
-                let work = x.avg*pattern[0].duration + y.avg*pattern[
-                        1].duration + z.avg*pattern[1].duration
-                if work > m:
-                    result = @[x, y, z]
-                    m = work
-
 proc select_all*(pattern: seq[Pattern], best: seq[seq[Interval]]): seq[Interval] =
 
     let total = pattern.zip(best).mapIt(it[1].len ^ it[0].repeat).prod()
@@ -170,38 +134,6 @@ proc select_all*(pattern: seq[Pattern], best: seq[seq[Interval]]): seq[Interval]
 
     step(@[], 0.0, pattern, best)
     return candidate
-
-proc select_one(last: int, pattern: Pattern, best: seq[Interval]): seq[Interval] =
-    var best = best
-    var repeat = pattern.repeat
-
-    while best.len > 0 and repeat > 0:
-        let x = best.pop()
-        if not(last <= x.start):
-            continue
-        if result.anyIt(overlap(it, x)):
-            continue
-        result.add(x)
-        repeat -= 1
-
-    if repeat > 0:
-        echo "not all interval found"
-        return @[]
-
-proc select_top_33*(pattern: seq[Pattern], best: seq[seq[Interval]]): seq[Interval] =
-    let p1 = select_one(0, pattern[0], best[0])
-    let p2 = select_one(p1[0].stop, pattern[1], best[1])
-    echo p2
-
-
-proc select_top*(pattern: seq[Pattern], best: seq[seq[Interval]]): seq[Interval] =
-    let count = pattern.map(x => x.repeat).sum()
-    if count == 1:
-        return pattern.select_top_1(best)
-    elif count == 2:
-        return pattern.select_top_2(best)
-    elif count == 3:
-        return pattern.select_top_3(best)
 
 proc process_old*(pattern: seq[Pattern], time: seq[float], watts: seq[
         float]): seq[Interval] =
